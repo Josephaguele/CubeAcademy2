4d26c5e52fd229faf81a98bc68293fe8935b7c63
Joseph <joseph.aguele@acklenavenue.com>
2023-10-28 13:08:25 +0100

    some little clean up

--- app/src/main/java/com/cube/cubeacademy/activities/CreateNominationActivity.kt
+++ app/src/main/java/com/cube/cubeacademy/activities/CreateNominationActivity.kt
@@ -65,16 +65,14 @@ class CreateNominationActivity : AppCompatActivity() {
 
         // Set the dialog background to be transparent
         formModal.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         leaveButton = formModal.findViewById(R.id.leave_button)
         cancelButton = formModal.findViewById(R.id.cancel_button)
+
         populateUI()
         multiColourTextView()
-
-        leaveButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        handleBackButton()
+        handleSubmitButton()
+        handleLeaveButton()
         cancelButton.setOnClickListener { formModal.dismiss() }
 
     }
@@ -92,7 +90,6 @@ class CreateNominationActivity : AppCompatActivity() {
             val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, nomineeNames)
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             nomineeSpinner.adapter = adapter
-
             // Set the nomineeList when it's available
             nomineeList = nominees
         })
@@ -103,13 +100,77 @@ class CreateNominationActivity : AppCompatActivity() {
         radioGroup.setOnCheckedChangeListener { _: RadioGroup, checkedId ->
             val radioButton = findViewById<RadioButton>(checkedId)
             processText = radioButton.text.toString().toLowerCase()
-            if (processText == "not sure") processText = "not_sure"
-            if (processText == "very fair") processText = "very_fair"
-            if (processText == "very unfair") processText = "very_unfair"
+            when (processText) {
+                "not sure" -> processText = "not_sure"
+                "very fair" -> processText = "very_fair"
+                "very unfair" -> processText = "very_unfair"
+            }
             checkEnableSubmitButton()
         }
 
 
+        // Observe the nomination result
+        viewModel.nominationResult.observe(this, Observer { result ->
+            Toast.makeText(
+                applicationContext,
+                "You submitted  on " + result?.dateSubmitted.toString(),
+                Toast.LENGTH_LONG
+            ).show()
+        })
+    }
+
+    private fun multiColourTextView() {
+        howWeRunTextView = binding.howwerunTextview
+        val spannableText = SpannableString(howWeRunTextView.text)
+        spannableText.setSpan(
+            ForegroundColorSpan(Color.parseColor(getString(R.string.red_colour))),
+            23,
+            howWeRunTextView.text.length - 5,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        howWeRunTextView.text = spannableText
+    }
+
+    private fun checkEnableSubmitButton() {
+        val nomineeSelected = nomineeList.isNotEmpty() && nomineeSpinner.selectedItemPosition != -1
+        val reasonNotEmpty = reasonEditText.text.toString().isNotBlank()
+        val processNotEmpty = processText.isNotBlank()
+        submitNominationButton.isEnabled = nomineeSelected && reasonNotEmpty && processNotEmpty
+    }
+
+    private fun navigateToMainActivity(){
+        val intent = Intent(this@CreateNominationActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showConfirmationDialog(){
+        val window = formModal.window
+        val wlp = window!!.attributes
+        wlp.gravity = Gravity.BOTTOM
+        wlp.flags = wlp.flags and WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        wlp.dimAmount = 0.7f // dim the background by 70%
+        // Set dialog width to match activity window width. I actually tried using xml with matchparent but it didn't work
+        // I will have to find out why later but for now this works. lol'
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        wlp.width = displayMetrics.widthPixels
+        window!!.attributes = wlp
+        formModal.show()
+    }
+
+    private fun handleBackButton(){
+        backButton.setOnClickListener {
+            val reasonNotEmpty = reasonEditText.text.toString().isNotBlank()
+            val processNotEmpty = processText.isNotBlank()
+            if (reasonNotEmpty || processNotEmpty) {
+                showConfirmationDialog()
+            } else {
+                navigateToMainActivity()
+            }
+        }
+    }
+
+    private fun handleSubmitButton() {
         submitNominationButton.setOnClickListener {
             if (nomineeList.isNotEmpty()) {
                 // Get the selected nominee's ID
@@ -132,60 +193,11 @@ class CreateNominationActivity : AppCompatActivity() {
                 ).show()
             }
         }
+    }
 
-        // Observe the nomination result
-        viewModel.nominationResult.observe(this, Observer { result ->
-            Toast.makeText(
-                applicationContext,
-                "You submitted  on " + result?.dateSubmitted.toString(),
-                Toast.LENGTH_LONG
-            ).show()
-
-        })
-
-
-
-        backButton.setOnClickListener {
-            val reasonNotEmpty = reasonEditText.text.toString().isNotBlank()
-            val processNotEmpty = processText.isNotBlank()
-            if (reasonNotEmpty || processNotEmpty) {
-                val window = formModal.window
-                val wlp = window!!.attributes
-                wlp.gravity = Gravity.BOTTOM
-                wlp.flags = wlp.flags and WindowManager.LayoutParams.FLAG_DIM_BEHIND
-                wlp.dimAmount = 0.7f // dim the background by 70%
-                // Set dialog width to match activity window width. I actually tried using xml with matchparent but it didn't work
-                // I will have to find out why later but for now this works. lol'
-                val displayMetrics = DisplayMetrics()
-                windowManager.defaultDisplay.getMetrics(displayMetrics)
-                wlp.width = displayMetrics.widthPixels
-                window!!.attributes = wlp
-                formModal.show()
-            } else {
-                val intent = Intent(this@CreateNominationActivity, MainActivity::class.java)
-                startActivity(intent)
-            }
+    private fun handleLeaveButton(){
+        leaveButton.setOnClickListener {
+            navigateToMainActivity()
         }
     }
-
-    private fun multiColourTextView() {
-        howWeRunTextView = binding.howwerunTextview
-        val spannableText = SpannableString(howWeRunTextView.text)
-        spannableText.setSpan(
-            ForegroundColorSpan(Color.parseColor(getString(R.string.red_colour))),
-            23,
-            howWeRunTextView.text.length - 5,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        howWeRunTextView.text = spannableText
-    }
-
-    private fun checkEnableSubmitButton() {
-        val nomineeSelected = nomineeList.isNotEmpty() && nomineeSpinner.selectedItemPosition != -1
-        val reasonNotEmpty = reasonEditText.text.toString().isNotBlank()
-        val processNotEmpty = processText.isNotBlank()
-        submitNominationButton.isEnabled = nomineeSelected && reasonNotEmpty && processNotEmpty
-    }
-
-
 }
